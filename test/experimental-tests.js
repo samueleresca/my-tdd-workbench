6979d0b02ad0a2b2e6297c1529c580aa82f564aa
Samuele Resca <samuele.resca@gmail.com>
2016-02-11 22:34:05 +0100

    First commit,jquery added

---
+++ test/experimental-tests.js
@@ -0,0 +1,43 @@
+
+//FIX to the error : "expect is not defined"
+var expect = chai.expect;
+
+
+//describe String tests
+describe('Global tests', function() {
+//Before the execution, initialize the object
+  before(function() {
+   });
+
+  it('console should be defined', function() {
+    expect(console).to.be.ok;
+  });
+
+  it('document should be defined', function() {
+    expect(document).to.be.ok;
+  });
+	  
+  it('JQuery should be defined', function() {
+    expect($).to.be.ok;
+  });
+
+});
+
+
+
+//describe String tests
+describe('String tests', function() {
+  var simple_url;
+//Before the execution, initialize the object
+  before(function() {
+     simple_url="http://www.localhost.it/system/selfservice.controller?"+
+     "CONFIGURATION=1375&PARTITION_ID=1&CMD=VIEW_ARTICLE&"+
+     "LANGUAGE=it&COUNTRY=it&USERTYPE=1&ARTICLE_ID=3621";
+
+  });
+  
+  it('URL should be contains the string', function() {
+    expect(simple_url.indexOf("ARTICLE_ID")).to.be.ok;
+  });
+});
+
